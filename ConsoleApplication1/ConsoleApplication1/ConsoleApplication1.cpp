91595353a68ec2d21155b2d8d0291fa9bf90c02f
Khgyr <45271571+Khgyr@users.noreply.github.com>
2018-11-23 23:01:43 +0400

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,8 +4,8 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-ifstream in("infile");
-ofstream out("outfile");
+ifstream in("infile.txt");
+ofstream out("outfile.txt");
 struct vedom {
 	int kol;
 	float ves;
@@ -14,36 +14,44 @@ struct vedom {
 	void output();
 };
 void vedom::show() {
-	cout << setw(3) << '\t' << fam << setw(3) << '\t' << im << setw(3) << '\t' << otch << setw(3) << '\t' << kol << setw(3) << '\t' << ves << endl;
+	cout <<'\t'<< setiosflags(ios::left) << setw(20) << fam << setw(20) << im << setw(20) << otch << setw(5) << kol << setw(5) << ves << endl;
 }
 void vedom::output() {
-	out << setw(3) << '\t' << fam << setw(3) << '\t' << im << setw(3) << '\t' << otch << setw(3) << '\t' << kol << setw(3) << '\t' << ves << endl;
+	out <<'\t'<< setiosflags(ios::left) << setw(20)<< fam << setw(20) << im << setw(20) << otch << setw(5)<< kol << setw(5) << ves << endl;
 }
 int main()
 {
 	vedom pass[50];
 	int v, n = 0, j = 0;
-	while (in.peek() != EOF) {
-		in >> pass[n].fam;
-		in >> pass[n].im;
-		in >> pass[n].otch;
-		in >> pass[n].kol;
-		in >> pass[n].ves;
-		++n;
-	}
-	for (int i = 0; i < n; ++i) {
-		pass[i].show();
-	}
-	cout << "Enter ves: "; cin >> v;
-	for (int i = 0; i < n && !j; ++i) {
-		if (pass[i].ves < v) {
-			for (j = i; j < n; ++j) {
-				pass[j] = pass[j + 1];
+	if (!in) { cout << "error opening infile\n"; }
+	else {
+		while (in.peek() != EOF) {
+			in >> pass[n].fam;
+			in >> pass[n].im;
+			in >> pass[n].otch;
+			in >> pass[n].kol;
+			in >> pass[n].ves;
+			++n;
+		}
+		--n;
+		for (int i = 0; i < n; ++i) {
+			pass[i].show();
+		}
+		cout << endl;
+		cout <<'\t'<< "Enter ves: "; cin >> v;
+		if (!out) { cout<<"error opening outfile\n"; }
+		else {
+			for (int i = 0; i < n && !j; ++i) {
+				if (pass[i].ves < v) {
+					for (j = i; j < n; ++j) {
+						pass[j] = pass[j + 1];
+					}
+					--n;
+				}
 			}
-			--n;
+			for (int i = 0; i < n; ++i) { pass[i].output(); }
 		}
 	}
-	for (int i = 0; i < n; ++i) { pass[i].output(); }
 	in.close();
 	out.close();
 	return 0;
